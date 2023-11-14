9d76b1754d7bbb6f63914d7062f1e328deab117c
Damien Ragoucy <dam@ramdam.io>
2023-11-14 09:21:36 +0100

    Add OCaml code

---
+++ ocaml_fibonacci/bin/main.ml
@@ -0,0 +1,16 @@
+let fib target =
+  let rec fib' n n_minus_1 n_minus_2 =
+    let sum = n_minus_1 + n_minus_2 in
+    if n = target
+    then sum
+    else fib' (n + 1) sum n_minus_1
+  in
+  match target with
+  | 0 -> 0
+  | 1 -> 1
+  | _ -> fib' 2 1 0
+
+let () =
+  let n = 40 in
+  let r = fib n in
+  Printf.printf "fib(%d) = %d\n%!" n r
